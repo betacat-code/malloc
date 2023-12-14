4aa2cfb872003352c20bf877a3bf36a682af99bd
betacat-code <62531793+betacat-code@users.noreply.github.com>
2023-12-14 10:34:28 +0800

    Update test_malloc.cpp

--- test_malloc.cpp
+++ test_malloc.cpp
@@ -1,1 +1,12 @@
-
+#include<iostream>
+#include<vector>
+#include"internal/ThreadCache.h"
+struct node {
+	int x, y;
+	char s;
+	std::string t;
+};
+int main(){
+  auto temp=alloca_obj<node>();//申请
+  free_obj(temp);
+}
